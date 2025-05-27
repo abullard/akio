5ece5f50f8e1ab3a40ba3377fc409502c58b77f8
root <root@WINTERMUTE>
2025-05-26 20:59:58 -0700

    got a test working lets goooo

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,20 +1,31 @@
 import { describe, it, expect } from "vitest";
 import { execa } from 'execa';
 
-describe('index.ts', () => {
-    it('should exec(akio) and pipe output', () => {
-        const cmd = "pnpm akio -i build";
-        const args = ['akio', '-i', 'build'];
-        const emptyOpts = {};
-
-
-        const { stdout } = await execa('pnpm', ['akio', '-i', 'builad']);
-        expect(stdout).toContain('Usage');
-        const expected = `
+const expected = `
 pnpm akio
 \t-----
 1. build      — 
-2. build:dev  — local dev buildaaaa
+2. build:dev  — local dev build
 3. build:prod —  build production fake
-        `;
+`;
+
+describe('index.ts', () => {
+    it('should run test:stub npm script', async () => {
+        const cmd = "pnpm";
+        const args = ['akio', 'test:stub'];
+
+        const subprocess = execa(cmd, args, {
+            stdin: 'pipe'
+        });
+
+        subprocess.stdin!.write('1\n');
+        subprocess.stdin!.end();
+
+        const { stdout } = await subprocess;
+        
+        console.log(stdout);
+        expect(stdout).toContain('text');
+
+        // expect(stdout).toMatchSnapshot(expected);
     });
+});
