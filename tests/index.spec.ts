fe304cafa7e68cdf442f20d42bca7f8cece71c38
root <root@WINTERMUTE>
2025-05-26 22:09:39 -0700

    more tests running and passing, introduced snapshots

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,14 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { execa } from 'execa';
 
-const expected = `
-pnpm akio
-\t-----
-1. build      — 
-2. build:dev  — local dev build
-3. build:prod —  build production fake
-`;
-
 describe('index.ts', () => {
     it('should run test:stub npm script', async () => {
         const cmd = "pnpm";
@@ -22,10 +14,32 @@ describe('index.ts', () => {
         subprocess.stdin!.end();
 
         const { stdout } = await subprocess;
-        
-        console.log(stdout);
-        expect(stdout).toContain('text');
+        expect(stdout).toMatchSnapshot();
+    });
 
-        // expect(stdout).toMatchSnapshot(expected);
+    it('should run search for build scripts', async () => {
+        const cmd = "pnpm";
+        const args = ['akio', '-i', 'build'];
+
+        const { stdout } = await execa(cmd, args, {});
+
+        expect(stdout).toMatchSnapshot();
+    });
+
+    it('should turn formatting off', async () => {
+        const cmd = "pnpm";
+        const args = ['akio', '-f'];
+
+        // TODO AJB 05/26/2025: make all of these tests normalized, and run one at a time
+        const { stdout } = await execa(cmd, args, {});
+
+        expect(stdout).toMatchSnapshot();
+    });
+
+    it('should error with wrong cli opts', async () => {
+        const cmd = "pnpm";
+        const args = ['akio', '-a'];
+
+        await expect(() => execa(cmd, args, {})).rejects.toThrow();
     });
 });
