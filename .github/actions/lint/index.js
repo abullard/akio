cdbc90aab96d216d7ab82b0c4411bf6fed4c2455
abullard <austinbullard@protonmail.ch>
2026-01-16 22:34:26 -0600

    introduced an error, moving too fast

--- .github/actions/lint/index.js
+++ .github/actions/lint/index.js
@@ -3,7 +3,7 @@ import * as core from '@actions/core';
 
 const run = () => {
     try {
-        execSync('npm run lint', { stdio: 'inherit', cwd });
+        execSync('npm run lint', { stdio: 'inherit', cwd: process.cwd() });
     } catch (e) {
         console.error(e);
         core.setFailed(`Linting Failed`);
