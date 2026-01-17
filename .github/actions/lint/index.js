118c263c23d207827176fbb8faa66ed832bb3504
abullard <austinbullard@protonmail.ch>
2026-01-16 21:28:44 -0600

    I\'m touching all kinds of files, jeez

--- .github/actions/lint/index.js
+++ .github/actions/lint/index.js
@@ -3,8 +3,10 @@ import * as core from '@actions/core';
 // import * as github from '@actions/github';
 
 const run = () => {
+    const cwd = process.cwd();
+    console.log('AJB: ', cwd);
     try {
-        execSync('pnpm lint', { stdio: 'inherit', cwd: process.cwd() });
+        execSync('npm run lint', { stdio: 'inherit', cwd });
     } catch (e) {
         console.error(e);
         core.setFailed(`Linting Failed`);
